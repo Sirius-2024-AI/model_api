7fab95c4a53d62e815b7b62c72c229e74c910fe9
Qwest1204 <workemailfordaniil@gmail.com>
2024-07-19 09:47:31 +0300

    INIT

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,24 @@
-from fastapi import FastAPI
-from starlette import status
-from starlette.responses import Response
+from fastapi import FastAPI, Request, Response
 
-from scr.utils import *
+from backend.scr.utils import *
 
 app = FastAPI()
 
-@app.get("/ping")
-def pong():
-    return {"ping": "pong!"}
+
+@app.get("/get_price")
+async def get_price(request:Request, response:Response,  city, area, kitchen_area, total_rooms, floor, total_floors, build_date, material_type,
+                    remont, total_balcony, metro_name, metro_dist_km,distance_to_center):
+    dict_data = {'city': city,
+                 'area': area,
+                 'kitchen_area': kitchen_area,
+                 'total_rooms': total_rooms,
+                 'floor': floor,
+                 'total_floors': total_floors,
+                 'build_date': build_date,
+                 'material_type': material_type,
+                 'remont': remont,
+                 'total_balcony': total_balcony,
+                 'metro_name': metro_name,
+                 'metro_dist_km': metro_dist_km,
+                 'distance_to_center': distance_to_center}
+    return {"price": price_predict(dict_data)}
